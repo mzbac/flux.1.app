dc3fed6c3706c93bb86c0b3634bb5c433ce63c53
anchen <li.anchen.au@gmail.com>
2024-10-10 00:35:02 +1100

    Initial Commit

---
+++ Flux.1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flux.1
+//
+//  Created by Anchen Li on 10/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flux.1/Flux_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Flux_1App.swift
+//  Flux.1
+//
+//  Created by Anchen Li on 10/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Flux_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
